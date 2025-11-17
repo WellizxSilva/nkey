53b88bb6923b0400dfd0e5f40f5f61c2d1a935a7
Wellington Silva <wellizxsilva@gmail.com>
2025-11-17 19:31:43 -0300

    test: add dispatch to FileStorage

--- tests/nkey.test.ts
+++ tests/nkey.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { Nkey } from "../src/core/Nkey";
-import { MemoryStorage } from "../src/core/storage/MemoryStorage";
+import { MemoryStorage, FileStorage } from "../src";
 
 type User = {
   name: string;
@@ -10,6 +10,15 @@ type User = {
 describe("Nkey<User> with MemoryStorage<User>", () => {
   it("should dispatch to memory", () => {
     const db = new Nkey<User>(new MemoryStorage<User>());
+    db.create("user:1", { name: "Wellizx" });
+    expect(db.read("user:1")).toEqual({ name: "Wellizx" });
+  });
+});
+
+describe("Nkey<User> with FileStorage<User>", () => {
+  it("should dispatch to file", () => {
+    const filePath = "./test-nkey.json";
+    const db = new Nkey<User>(new FileStorage<User>(filePath));
     db.create("user:1", { name: "Wellizx" });
     expect(db.read("user:1")).toEqual({ name: "Wellizx" });
   });
