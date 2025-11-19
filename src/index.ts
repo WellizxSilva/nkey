dfd7ba443fd1cfd0223dc979b0432e060ee48ecf
Wellington Silva <wellizxsilva@gmail.com>
2025-11-18 23:01:18 -0300

    update exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export type { IStorage } from "./core/storage/IStorage";
 export { DatabaseError } from "./errors/DatabaseError";
 export { KeyNotFoundError } from "./errors/KeyNotFoundError";
 export { KeyAlreadyExistsError } from "./errors/KeyAlreadyExistsError";
+export { NoStorageProvidedError } from "./errors/NoStorageProvidedError";
 export { MemoryStorage } from "./core/storage/MemoryStorage";
 export { FileStorage } from "./core/storage/FileStorage";
